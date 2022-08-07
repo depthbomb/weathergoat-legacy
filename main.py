a00c74fbd4181c320c3e37aacbca442b525bdf17
depthbomb <reesepup77@hotmail.com>
2022-08-07 18:26:45 -0500

    refactor: remove `schedule` dep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import time
-import schedule
 from sys import exit
 from logger import logger
 from config import config
@@ -16,18 +15,13 @@ def check_weather_alerts() -> None:
 if __name__ == "__main__":
     logger.info("Starting")
 
-    check_weather_alerts()
-
     interval = int(config["weather"]["check_interval"])
     should_loop = True
-    job = schedule.every(interval).minutes.do(check_weather_alerts)
     while should_loop:
         try:
-            schedule.run_pending()
-            time.sleep(1)
+            check_weather_alerts()
+            time.sleep(interval)
         except KeyboardInterrupt:
-            logger.info("Stopping job")
             should_loop = False
-            schedule.cancel_job(job)
 
     exit(0)
